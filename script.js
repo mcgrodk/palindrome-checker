dbf145240bc52402598857ba9772272c2974d014
mcgrodk <mcgroderk@gmail.com>
2025-03-07 21:18:20 -0800

    Added definition div, updated script; palindrome checker fully functional

--- script.js
+++ script.js
@@ -4,22 +4,34 @@ const result = document.getElementById("result");
 
 function checkForPalindrome() {
   const userInput = textInput.value;
-  const userInputArray = [...userInput];
-  const forwardArray = userInputArray.filter(char => char == /[a-z0-9]/)
-  console.log(userInputArray);
-  console.log(forwardArray);
-}
 
-function displayResult() {
-  const userInput = textInput.value;
-  console.log(textInput.value);
-  
-  if (userInput === "") {
-    alert("Please input a value")
-  } else {
-    result.style.display = "block";
-    result.textContent = `You entered ${userInput}.`;
+  if (!userInput) {
+    alert("Please input a value");
+    result.style.display = "none";
+    return;
   }
+
+  // filter() executes a function on each element in the array;
+  // test() is used to check if a variable matches a regex
+  const forwardArray = [...userInput.toLowerCase()].filter(char => /^[0-9a-z]$/.test(char));
+  const backwardArray = [...forwardArray].reverse();
+
+  let isPalindrome = true;
+
+  for (let i = 0; i < forwardArray.length; i++) {
+    if (forwardArray[i] !== backwardArray[i]) {
+      isPalindrome = false;
+      break;
+    }
+  }
+
+  if (isPalindrome) {
+    result.textContent = `${userInput} is a palindrome.`;
+  } else {
+    result.textContent = `${userInput} is not a palindrome.`;
+  }
+
+  result.style.display = "block";
 }
 
 checkBtn.addEventListener("click", checkForPalindrome);
